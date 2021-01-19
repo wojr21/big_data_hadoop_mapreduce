2418f27b5db1e129ffa43abeb956246805ca2cb8
Wojtek <wojr2116@gmail.com>
2021-01-19 14:59:35 +0100

    Taxi 1

---
+++ 05_taxi/02_passenger_count_distribution.py
@@ -0,0 +1,16 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class MRTaxi(MRJob):
+
+    def mapper(self, _, line):
+        (VendorID, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count, trip_distance, pickup_longitude,
+         pickup_latidute, RatecodeID, store_and_fwd_flag, dropoff_longitude, dropoff_latidute, payment_type,
+         fare_amount, extra, mta_tax, tip_amount, tolls_amount, improvement_surcharge, total_amount) = line.split(',')
+        yield passenger_count, int(passenger_count)
+
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+if __name__ == '__main__':
+    MRTaxi.run()
